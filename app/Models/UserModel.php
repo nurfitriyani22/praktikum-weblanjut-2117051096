2645285c30b5c2e4327435b8cd80feff3ddc1dd7
inimicin <osmycin.amelia21@students.unila.ac.id>
2023-10-28 23:30:13 +0700

    modul 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -53,4 +53,12 @@ class UserModel extends Model
         return $this->select('user.*, kelas.nama_kelas')
             ->join('kelas', 'kelas.id=user.id_kelas')->findAll();
     }
+
+    public function updateUser($data, $id){
+        return $this->update($id, $data);
+    }
+
+    public function deleteUser($id){
+        return $this->delete($id);
+    }
 }
--- app/Views/list_user.php
+++ app/Views/list_user.php
@@ -12,7 +12,10 @@
         </tr>
     </thead>
     <tbody>
-    <a href="<?= base_url('/user/create') ?>">Tambah Data</a>
+    <!-- <a href="<?= base_url('/user/create') ?>">Tambah Data</a> -->
+    <form action="<?= base_url('/user/create') ?>">
+    <button type=submit>Tambah Data</button></form>
+
         <?php
         foreach ($users as $user){
         ?>
@@ -22,10 +25,14 @@
             <td><?= $user['npm'] ?></td>
             <td><?= $user['nama_kelas'] ?></td>
             <td>
-                <a href="<?= base_url('user/' . $user['id']) ?>">Detail</a>
-                <!-- <button type=button>Detail</button> -->
-                <a href="<?= base_url('user/' . $user['id'] . '/edit') ?>">Edit</a>
-                <form action="<?= base_url('user/' . $user['id']) ?>" method="post"><button type=button>Hapus</button>
+                <form action="<?= base_url('user/' . $user['id']) ?>" method="get">
+                <button type=submit>Detail</button></form>
+                <form action="<?= base_url('user/' . $user['id'] . '/edit') ?>" method="get">
+                <button type=submit>Edit</button></form>
+                <form action="<?= base_url('user/' . $user['id']) ?>" method="post">
+                <input type="hidden" name="_method" value="DELETE">
+                <?= csrf_field() ?>
+                <button type=submit>Hapus</button></form>
             </td>
         </tr>
         <?php
